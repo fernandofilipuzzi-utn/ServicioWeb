ff61c02b7675dbb1b73983b3fb24d9491fd2dd96
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-01-19 07:42:35 -0300

    renombrando y ordenando

---
+++ ServicioWebAPI_MOVILE/ServicioEncuestasASP_API/Controllers/EjController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServicioEncuestas.Controllers
+{
+    public class EjController : ApiController
+    {
+        // GET: api/Ej
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Ej/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Ej
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Ej/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Ej/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ServicioWebAPI_MOVILE/ServicioEncuestasASP_API/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace ServicioEncuestas
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Código que se ejecuta al iniciar la aplicación
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
